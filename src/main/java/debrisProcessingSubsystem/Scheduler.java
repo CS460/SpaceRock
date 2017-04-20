454dbfeb7633a657c048ab669f7da130be890623
Neil Sparks <nsparks@unm.edu>
2017-04-20 13:28:10 -0600

    Cleanup and debug that works

--- src/main/java/debrisProcessingSubsystem/Scheduler.java
+++ src/main/java/debrisProcessingSubsystem/Scheduler.java
@@ -9,17 +9,17 @@ import debrisProcessingSubsystem.updateSystem.UpdateType;
 
 /**
  * @author Deb Rezanka
- *
- * The Scheduler will interface with the DebrisCollection, Operator,
- * and Camera objects. The Scheduler acts as a shuttle for data and
- * messages between these objects and knows nothing of the contents of the
- * Update packages.
- *
- * The Operator is sent all Updates that are not specifically for the camera or
- * the debrisCollector. Therefore the operator class will need to deal with
- * non-standard Update types.
- *
- * Created by dsr on 3/4/17.
+ *         <p>
+ *         The Scheduler will interface with the DebrisCollection, Operator,
+ *         and Camera objects. The Scheduler acts as a shuttle for data and
+ *         messages between these objects and knows nothing of the contents of the
+ *         Update packages.
+ *         <p>
+ *         The Operator is sent all Updates that are not specifically for the camera or
+ *         the debrisCollector. Therefore the operator class will need to deal with
+ *         non-standard Update types.
+ *         <p>
+ *         Created by dsr on 3/4/17.
  */
 public class Scheduler
 {
@@ -46,23 +46,8 @@ public class Scheduler
     worker = new Worker();
     Thread t = new Thread(worker);
     t.setDaemon(true);
-    if(DEBUG) System.out.println("Scheduler started");
+    if (DEBUG) System.out.println("Scheduler started");
     t.start();
-  }
-
-  private class Worker implements Runnable
-  {
-    @Override
-    public void run()
-    {
-      //continuous loop
-      while (runScheduler)
-      {
-//        check_Collection();
-        check_Operator();
-        check_Camera();
-      }
-    }
   }
 
   /**
@@ -70,25 +55,24 @@ public class Scheduler
    * sends them. It will continue until the operator has no more updates to
    * send (allows constant stream of commands to be sent to the camera)
    * If the Update reply is not null the inner loop will handle sending those Updates.
-   *
+   * <p>
    * A separate sendUpdate() method is used to determine where the Update is sent.
-   *
+   * <p>
    * The method returns when responseUpdate and returnedUpdate are both null
-   *
    */
   private void check_Operator()
   {
     while ((returnedUpdate = operator.pollComponent()) != null)
     {
       //stop scheduler
-      if(returnedUpdate.getUpdateType() == UpdateType.STOP_SCHEDULER)
+      if (returnedUpdate.getUpdateType() == UpdateType.STOP_SCHEDULER)
       {
         runScheduler = false;
-        if(DEBUG) System.out.println("Scheduler Stopped");
+        if (DEBUG) System.out.println("Scheduler Stopped");
         return;
       }
       //pause scheduler if communication is down.
-      if(returnedUpdate.getUpdateType() == UpdateType.COMMUNICATION_DOWN)
+      if (returnedUpdate.getUpdateType() == UpdateType.COMMUNICATION_DOWN)
       {
         if (DEBUG) System.out.println("Scheduler received COMMUNICATION_DOWN");
         try
@@ -101,7 +85,7 @@ public class Scheduler
             while (returnedUpdate != null && returnedUpdate.getUpdateType() != UpdateType.COMMUNICATION_UP)
             {
               returnedUpdate = sendUpdate(returnedUpdate);
-              if(DEBUG) System.out.println("Scheduler sent update: " + returnedUpdate.getUpdateType());
+              if (DEBUG) System.out.println("Scheduler sent update: " + returnedUpdate.getUpdateType());
             }
             wait(10_000);
             returnedUpdate = operator.pollComponent();
@@ -114,15 +98,41 @@ public class Scheduler
       }
       //resume scheduler if communication us up
       responseUpdate = sendUpdate(returnedUpdate);
-      if(DEBUG) System.out.println("Scheduler sent update: " + returnedUpdate.getUpdateType());
+      if (DEBUG) System.out.println("Scheduler sent update: " + returnedUpdate.getUpdateType());
 
       while (responseUpdate != null)
       {
         responseUpdate = sendUpdate(responseUpdate);
-        if(DEBUG) System.out.println("Scheduler sent update: " + responseUpdate.getUpdateType());
+        if (DEBUG) System.out.println("Scheduler sent update: " + responseUpdate.getUpdateType());
       }
     }
   }
+
+  /**
+   * check_Camera() polls the camera for update packages and
+   * sends them. It will continue until the camera has no more updates to
+   * send (allows constant stream of debris data to be sent to the debris collecor)
+   * If the Update reply is not null the inner loop will handle sending those Updates.
+   * <p>
+   * A separate sendUpdate() method is used to determine where the Update is sent.
+   * <p>
+   * The method returns when responseUpdate and returnedUpdate are both null
+   */
+  private void check_Camera()
+  {
+    while ((returnedUpdate = camera.pollComponent()) != null)
+    {
+      responseUpdate = sendUpdate(returnedUpdate);
+      if (DEBUG) System.out.println("Scheduler sent update: " + returnedUpdate.getUpdateType());
+
+      while (responseUpdate != null)
+      {
+        responseUpdate = sendUpdate(responseUpdate);
+        if (DEBUG) System.out.println("Scheduler sent update: " + responseUpdate.getUpdateType());
+      }
+    }
+  }
+
 //  /**
 //   * check_Collection() polls the debrisCollector for update packages and
 //   * sends them. It will continue until the debrisCollector has no more updates to
@@ -149,35 +159,8 @@ public class Scheduler
 //  }
 
   /**
-   * check_Camera() polls the camera for update packages and
-   * sends them. It will continue until the camera has no more updates to
-   * send (allows constant stream of debris data to be sent to the debris collecor)
-   * If the Update reply is not null the inner loop will handle sending those Updates.
-   *
-   * A separate sendUpdate() method is used to determine where the Update is sent.
-   *
-   * The method returns when responseUpdate and returnedUpdate are both null
-   */
-  private void check_Camera()
-  {
-    while ((returnedUpdate = camera.pollComponent()) != null)
-    {
-      responseUpdate = sendUpdate(returnedUpdate);
-      if(DEBUG) System.out.println("Scheduler sent update: " + returnedUpdate.getUpdateType());
-
-      while (responseUpdate != null)
-      {
-        responseUpdate = sendUpdate(responseUpdate);
-        if(DEBUG) System.out.println("Scheduler sent update: " + responseUpdate.getUpdateType());
-      }
-    }
-  }
-
-  /**
-   *
    * @param theUpdate Update package to send
    * @return response Update package from the receiver
-   *
    */
   public Update sendUpdate(Update theUpdate)
   {
@@ -186,41 +169,58 @@ public class Scheduler
     if (theUpdate.getUpdateType() == UpdateType.CAMERA)
     {
       response = camera.updateComponent(theUpdate);
-      if(DEBUG) System.out.println("Update Type = CameraUpdate");
+      if (DEBUG) System.out.println("Update Type = CameraUpdate");
 
     }
     else if (theUpdate.getUpdateType() == UpdateType.DEBRIS_COLLECTOR)
     {
       response = debrisCollection.updateComponent(theUpdate);
-      if(DEBUG) System.out.println("Update Type = DebrisCollectorUpdate");
+      if (DEBUG) System.out.println("Update Type = DebrisCollectorUpdate");
     }
     else
     {
       //any odd messages, errors etc. go to the operator
       //the operator will need to deal with unknown Update types.
-      
-      if(DEBUG)
+
+      if (DEBUG)
       {
         operator = new OperatorTesting();
-        
+
       }
       responseUpdate = operator.updateComponent(theUpdate);
       System.out.println("Update Type = OperatorUpdate");
     }
     return response;
   }
+
   //For testing purposes
   public Updatable getCamera()
   {
     return camera;
   }
+
   public Updatable getDebrisCollection()
   {
     return debrisCollection;
   }
+
   public Updatable getOperator()
   {
     return operator;
   }
 
+  private class Worker implements Runnable
+  {
+    @Override
+    public void run()
+    {
+      //continuous loop
+      while (runScheduler)
+      {
+//        check_Collection();
+        check_Operator();
+        check_Camera();
+      }
+    }
+  }
 }
