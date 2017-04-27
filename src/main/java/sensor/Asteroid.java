aef0ec3a698b08ce32c13e613b7428462274381b
Neil Sparks <nsparks@unm.edu>
2017-04-26 23:08:30 -0600

    Now displays raw image. Working on BG color for cleanliness.

--- src/main/java/sensor/Asteroid.java
+++ src/main/java/sensor/Asteroid.java
@@ -1,5 +1,8 @@
 package sensor;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -9,35 +12,28 @@ import java.util.Random;
 /**
  * Created by Sean on 2/4/17.
  */
-public class Asteroid {
+public class Asteroid
+{
   private static final int[] size_range = {10, 100};
   private static final int[] distance_range = {100, 1000};
   private static final int[] speed_range = {0, 50};
   private static final String[] asteroid_images = {"asteroid_1.png", "asteroid_2.png", "asteroid_3.png",
-          "asteroid_4.png", "asteroid_5.png"};
+    "asteroid_4.png", "asteroid_5.png"};
 
   private static int nextid = 0;
-
+  public int[] current_location;
+  public int current_radius;
   private int[] initial_location;
   private int size;
   private int[] trajectory;
   private int id;
   private BufferedImage image;
-  private int created_at;
   //private HashMap<Integer, Integer[]> hist_location;
+  private Image fximage;
+  private int created_at;
 
-  public int[] current_location;
-  public int current_radius;
-
-  public int getSize(){return this.size;}
-
-  public int[] getStartingLocation(){return initial_location;}
-
-  public int[] getTrajectory(){return trajectory;}
-
-  public void setID(int id){this.id = id;}
-
-  public Asteroid(int[] initial_location, int size, int[] trajectory, int elapsed_time) {
+  public Asteroid(int[] initial_location, int size, int[] trajectory, int elapsed_time)
+  {
     this.initial_location = initial_location;
     this.size = size;
     this.trajectory = trajectory;
@@ -47,7 +43,8 @@ public class Asteroid {
     setRandomImage();
   }
 
-  public Asteroid(int[] loc_constraint, int elapsed_time) {
+  public Asteroid(int[] loc_constraint, int elapsed_time)
+  {
     Random rand = new Random();
     this.initial_location = new int[]{loc_constraint[0], loc_constraint[1], rand.nextInt(distance_range[1]) + distance_range[0]};
     this.size = rand.nextInt(size_range[1]) + size_range[0];
@@ -60,53 +57,89 @@ public class Asteroid {
     setRandomImage();
   }
 
-  public int getId() {
+  public int getSize()
+  {
+    return this.size;
+  }
+
+  public int[] getStartingLocation()
+  {
+    return initial_location;
+  }
+
+  public int[] getTrajectory()
+  {
+    return trajectory;
+  }
+
+  public void setID(int id)
+  {
+    this.id = id;
+  }
+
+  public int getId()
+  {
     return this.id;
   }
 
-  public void move(int elapsed_seconds) {
+  public void move(int elapsed_seconds)
+  {
     this.current_location = location(elapsed_seconds);
     //this.current_radius = radius(elapsed_seconds);
   }
 
-  public int[] location(int elapsed_seconds) {
+  public int[] location(int elapsed_seconds)
+  {
     return new int[]{current_location[0] + trajectory[0] * elapsed_seconds,
-                     current_location[1] + trajectory[1] * elapsed_seconds,
-                     current_location[2] + trajectory[2] * elapsed_seconds};
+      current_location[1] + trajectory[1] * elapsed_seconds,
+      current_location[2] + trajectory[2] * elapsed_seconds};
   }
 
-  public int radius(int elapsed_seconds) {
-    if (location(elapsed_seconds)[2] == 0) {
+  public int radius(int elapsed_seconds)
+  {
+    if (location(elapsed_seconds)[2] == 0)
+    {
       return 4000;
     }
-    return (int)((size / 2.0) / (location(elapsed_seconds)[2] / 100.0));
+    return (int) ((size / 2.0) / (location(elapsed_seconds)[2] / 100.0));
   }
 
-  public BufferedImage getImage() {
+  public BufferedImage getImage()
+  {
     return image;
+  }
+  public Image getFXImage()
+  {
+    return fximage;
   }
 
   @Override
-  public String toString() {
+  public String toString()
+  {
     return "Asteroid{" +
-        "id=" + id +
-        ", initial_location=" + Arrays.toString(initial_location) +
-        ", size=" + size +
-        ", trajectory=" + Arrays.toString(trajectory) +
-        ", created_at=" + created_at +
-        ", current_location=" + Arrays.toString(current_location) +
-        ", current_radius=" + current_radius +
-        '}';
+      "id=" + id +
+      ", initial_location=" + Arrays.toString(initial_location) +
+      ", size=" + size +
+      ", trajectory=" + Arrays.toString(trajectory) +
+      ", created_at=" + created_at +
+      ", current_location=" + Arrays.toString(current_location) +
+      ", current_radius=" + current_radius +
+      '}';
   }
 
-  private void setRandomImage() {
+  private void setRandomImage()
+  {
     Random rand = new Random();
     String asteroid_image = asteroid_images[rand.nextInt(asteroid_images.length)];
     System.out.format("setting imageView to: %s\n", asteroid_image);
-    try {
-
+    try
+    {
       this.image = ImageIO.read(Asteroid.class.getResource("asteroids/" + asteroid_image));
-    } catch (IOException e) {
+      System.out.println("asteroids/" + asteroid_image);
+      this.fximage = SwingFXUtils.toFXImage(this.image,null);
+    }
+    catch (IOException e)
+    {
     }
   }
 }
--- src/main/java/sensor/Universe/SpaceRockFXMLController.java
+++ src/main/java/sensor/Universe/SpaceRockFXMLController.java
@@ -1,13 +1,14 @@
 package sensor.Universe;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
@@ -15,7 +16,6 @@ import java.util.ResourceBundle;
 
 
 /**
- *
  * @author keira
  */
 public class SpaceRockFXMLController implements Initializable
@@ -38,11 +38,18 @@ public class SpaceRockFXMLController implements Initializable
 
   @FXML
   private Label diameter;
+
+  @FXML
+  private ImageView imgView;
+
   private long ID;
   private Instant timestamp;
   private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+  private Image image;
 
-  public void setData(sensor.Asteroid asteroid) {
+  public void setData(sensor.Asteroid asteroid)
+  {
+    this.image = asteroid.getFXImage();
     this.size = asteroid.current_radius;
     this.ID = asteroid.getId();
     //this.timestamp = asteroid.timestamp;
@@ -50,18 +57,23 @@ public class SpaceRockFXMLController implements Initializable
     objectID.setText("" + ID);
     //timeCaptured.setText("Timestamp: " + timestamp.toString());
   }
+
   @FXML
   public void buttonPressed(ActionEvent e)
   {
-    if(e.getSource() == requestFrameButton)
+    if (e.getSource() == requestFrameButton)
     {
       System.out.println("Raw frame requested!");
+      System.out.println();
+      imgView.setStyle("-fx-background-color: BLACK");
+      imgView.setImage(this.image);
     }
   }
 
 
   @Override
-  public void initialize(URL url, ResourceBundle rb) {
+  public void initialize(URL url, ResourceBundle rb)
+  {
 
 //        zoom_txt.setText(Double.toString(0.000));
 //        section_size_txt.setText(Double.toString(100.0));
@@ -77,7 +89,6 @@ public class SpaceRockFXMLController implements Initializable
 //            overlap_amount_txt.setText(Double.toString((overlap_amount_slide.getValue()) * 2));
 //        });
   }
-
 
 
 }
